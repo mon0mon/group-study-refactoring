2fd89f23c8c818f1cd50a3d8574cc9a9a34beb82
mon0mon <mon0mon@naver.com>
2025-02-16 11:08:46 +0900

    docs : Chapter06 기본적인 리팩터링 - 6.7 변수 이름 바꾸기

---
+++ src/main/kotlin/ch06/sec07/Ch_06_07_01.kt
@@ -0,0 +1,9 @@
+package ch06.sec07
+
+private fun unnamedFunction(height: Double, width: Double) {
+    val a = height * width
+}
+
+private fun unnamedFunction_Refactored(height: Double, width: Double) {
+    val area = height * width
+}
